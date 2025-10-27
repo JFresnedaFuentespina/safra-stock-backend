57b3123a1f99c8c8a82d801ba194665d43550cc6
JFresnedaFuentespina <jfresnedafuentespina@gmail.com>
2025-10-27 20:21:43 +0100

    eliminar linea innecesaria

--- src/main/java/com/safra/stock/safra_stock/controllers/CocinaCentralController.java
+++ src/main/java/com/safra/stock/safra_stock/controllers/CocinaCentralController.java
@@ -9,7 +9,6 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
