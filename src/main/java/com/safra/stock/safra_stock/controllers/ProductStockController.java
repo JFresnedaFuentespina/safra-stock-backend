9906db13ee5ca04b0ce6f899f1b9cbd9f86b96df
JFresnedaFuentespina <jfresnedafuentespina@gmail.com>
2026-02-18 19:42:24 +0100

    mostrar stock a editar

--- src/main/java/com/safra/stock/safra_stock/controllers/ProductStockController.java
+++ src/main/java/com/safra/stock/safra_stock/controllers/ProductStockController.java
@@ -10,7 +10,11 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.safra.stock.safra_stock.entities.ProductStock;
@@ -18,11 +22,6 @@ import com.safra.stock.safra_stock.entities.ProductStockDate;
 import com.safra.stock.safra_stock.services.ProductStockService;
 
 import jakarta.validation.Valid;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/safra-stock/stock")
@@ -36,12 +35,15 @@ public class ProductStockController {
         return service.findAll();
     }
 
-    @GetMapping("/{localName}/{date}")
-    public List<ProductStockDate> getStock(
-            @PathVariable String localName,
-            @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+    @GetMapping("/get-local-stock")
+    public ResponseEntity<?> getStock(
+            @RequestParam String localName,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+
         System.out.println("INTENTANDO ACCEDER AL STOCK DE " + localName + " DEL DIA " + date);
-        return service.findByLocalAndDate(localName, date);
+
+        return ResponseEntity.ok(
+                service.findByLocalAndDate(localName, date));
     }
 
     @PostMapping()
